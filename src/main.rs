051d62977719b29f37f32f2e03f3509362784f4a
Alexander Jackson <alexanderjackson@protonmail.com>
2021-01-02 12:37:04 +0000

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Args {
+    input: String,
+    output: String,
+}
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn parse_args() -> Result<Args> {
+    let mut args = pico_args::Arguments::from_env();
+
+    let args = Args {
+        input: args.value_from_str("--input")?,
+        output: args.value_from_str("--output")?,
+    };
+
+    Ok(args)
+}
+
+fn main() -> Result<()> {
+    let args = parse_args()?;
+
+    Ok(())
 }

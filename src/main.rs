8e5024e09d8747a55394ec8be3af2fa6ff661293
Alexander Jackson <alexanderjackson@protonmail.com>
2021-01-02 12:52:03 +0000

    Add basic deserialisation and formatting
    Add a basic Deserialize derive and read the input file, allowing it to
    be reformatted with only certain cells in the output.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
+use std::fmt;
+use std::fs::File;
+use std::io::BufReader;
+
+#[macro_use]
+extern crate serde;
+
 #[derive(Debug)]
 struct Args {
     input: String,
     output: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Notebook {
+    cells: Vec<NotebookCell>,
+}
+
+#[derive(Debug, Deserialize)]
+struct NotebookCell {
+    cell_type: String,
+    source: Vec<String>,
+}
+
+impl fmt::Display for Notebook {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for cell in self.cells.iter().filter(|c| c.cell_type != "markdown") {
+            for line in &cell.source {
+                write!(f, "{}", line)?;
+            }
+        }
+
+        Ok(())
+    }
 }
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -19,6 +49,13 @@ fn parse_args() -> Result<Args> {
 
 fn main() -> Result<()> {
     let args = parse_args()?;
+
+    // Read the input file and decode it
+    let input = File::open(&args.input)?;
+    let reader = BufReader::new(input);
+    let notebook: Notebook = serde_json::from_reader(reader)?;
+
+    println!("{}", &notebook);
 
     Ok(())
 }
